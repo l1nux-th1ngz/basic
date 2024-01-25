ab0f4b4bcad4a3a324184835ac665e263eb7e672
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-01-24 21:10:14 -0700

    Create base.sh

---
+++ base.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install dependencies
+yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus
+
+# Pause for a second
+sleep 1
+
+# Install additional packages
+yay -S hyprland waybar kitty firefox
